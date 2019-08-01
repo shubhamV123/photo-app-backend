27cade8de51f845744b9c42bd9cb9b5dde6d94e4
Shubham <sv.code97@gmail.com>
2019-08-01 16:51:09 +0530

    added upload code functionality

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cloudinary = require('./config/cloudinary');
+
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(fileUpload());
+
+const port = 5000 || process.env.PORT;
+
+//Code
+app.post('/upload', function (req, res) {
+    if (Object.keys(req.files).length == 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+    let getFileFromClient = req.files.file;
+    let fileName = req.files.file.name;
+    //Move file and upload it to cloudinary
+    getFileFromClient.mv(__dirname + '/images/' + fileName, function (err) {
+        if (err)
+            return res.status(500).send(err);
+
+        cloudinary.uploader.upload(__dirname + '/images/' + fileName, function (err, result) {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            let { width, height, url } = result;
+            res.json({
+                width, height, url
+            });
+        });
+    });
+
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
